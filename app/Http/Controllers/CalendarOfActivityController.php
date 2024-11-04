32720cb3dd1824424b290ca020a48378cef25db5
Alfeh Rae <chaimelenjambre@gmail.com>
2024-11-05 06:40:33 +0800

    update

--- app/Http/Controllers/CalendarOfActivityController.php
+++ app/Http/Controllers/CalendarOfActivityController.php
@@ -14,31 +14,28 @@ class CalendarOfActivityController extends Controller
     public function __invoke(Request $request)
     {
         $activities = Activity::where('status', 1)
+            ->where('is_deleted', 0)
+            ->where('type', '!=', 2) 
             ->get()
             ->filter(function ($activity) {
                 $endDate = Carbon::parse($activity->end_date);
-                // Exclude activities that have already ended
                 return Carbon::now()->lte($endDate);
             })
             ->map(function ($activity) {
                 $startDate = Carbon::parse($activity->start_date);
                 $endDate = Carbon::parse($activity->end_date);
 
-                // Check if the end date is earlier than the start date
                 if ($endDate->lt($startDate)) {
-                    // Set the end date to one hour after the start date if it's earlier
                     $endDate = $startDate->copy()->addHour();
                 }
 
                 return [
                     'title' => $activity->title,
-                    'start' => $startDate->format('Y-m-d H:i:s'), // Format start date
-                    'end' => $endDate->format('Y-m-d H:i:s'), // Format end date
+                    'start' => $startDate->format('Y-m-d H:i:s'), 
+                    'end' => $endDate->format('Y-m-d H:i:s'), 
                 ];
             });
 
-        // Return the view with the mapped activities
         return view('admin-sport.calendar-of-activity.index', ['activities' => $activities]);
     }
-
 }
