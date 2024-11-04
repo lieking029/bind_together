1a0c5dcd62d09f2aa16c4390ec668e578d85cc52
Dilany08 <dannelramos0802@gmail.com>
2024-11-04 22:31:33 +0800

    design

--- resources/views/layouts/guest-topbar.blade.php
+++ resources/views/layouts/guest-topbar.blade.php
@@ -1,4 +1,5 @@
 <style>
+     @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap');
     /* Top bar styling */
     .topbar {
         background-color: #800000;
@@ -50,8 +51,87 @@
         font-weight: 600;
         color: #333;
     }
-</style>
 
+    @media (min-width: 1200px) {
+  .navmenu {
+    padding: 0;
+  }
+
+  .navmenu ul {
+    margin: 0;
+    padding: 0;
+    display: flex;
+    list-style: none;
+    align-items: center;
+  }
+
+  .navmenu li {
+    position: relative;
+  }
+
+  .navmenu>ul>li {
+    white-space: nowrap;
+    padding: 15px 14px;
+  }
+
+  .navmenu>ul>li:last-child {
+    padding-right: 0;
+  }
+
+  .navmenu a,
+  .navmenu a:focus {
+    color: #2c4964;
+    font-size: 15px;
+    padding: 0 2px;
+    font-family: "Raleway",  sans-serif;
+    font-weight: 400;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    white-space: nowrap;
+    transition: 0.3s;
+    position: relative;
+  }
+
+  .navmenu a i,
+  .navmenu a:focus i {
+    font-size: 12px;
+    line-height: 0;
+    margin-left: 5px;
+    transition: 0.3s;
+  }
+
+  .navmenu>ul>li>a:before {
+    content: "";
+    position: absolute;
+    width: 100%;
+    height: 2px;
+    bottom: -6px;
+    left: 0;
+    background-color: #800000;
+    visibility: hidden;
+    width: 0px;
+    transition: all 0.3s ease-in-out 0s;
+  }
+
+  .navmenu a:hover:before,
+  .navmenu li:hover>a:before,
+  .navmenu .active:before {
+    visibility: visible;
+    width: 100%;
+  }
+
+  .navmenu li:hover>a,
+  .navmenu .active,
+  .navmenu .active:focus {
+    color: #800000;
+  }
+
+}   
+</style>
+ <?php
+$current_page = basename($_SERVER['PHP_SELF']);
+?>
 <div class="topbar text-center text-md-start">
     <div class="container">
         <i class="bi bi-envelope"></i> <a href="mailto:bpsu.bindtogether@gmail.com"
@@ -61,26 +141,16 @@
 <header class=" bg-white shadow-sm">
     <div class="container d-flex align-items-center justify-content-between">
         <!-- Logo and Site Name -->
-        <a href="#" class="logo d-flex align-items-center">
+        <a href="#" class="logo navbar-expand-md d-flex align-items-center" style="text-decoration: none; ">
             <img src="{{ asset('images/bindtogether-logo.png') }}" alt="BPSU Logo">
-            <h1 class="sitename">Bataan Peninsula State University</h1>
+            <h1 class="sitename" style="font-family: 'Poppins', sans-serif; font-weight: 700; color: #2c4964; font-size: 25px;">Bataan Peninsula State University</h1>
         </a>
 
         <!-- Navigation -->
-        <nav class="navbar navbar-expand-md">
-            <ul class="navbar-nav me-auto">
-                <li class="nav-item">
-                    <a href="#" class="nav-link active">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a href="#" class="nav-link">About</a>
-                </li>
-                <li class="nav-item">
-                    <a href="#" class="nav-link">Sports/Organization</a>
-                </li>
-                <li class="nav-item">
-                    <a href="#" class="nav-link">Contact</a>
-                </li>
+        <nav  id="navmenu" class="navmenu" style="font-family: 'Poppins', sans-serif;">
+            <ul >
+                <li><a href="#" style="margin-left: 70vh; text-decoration:none;">Home</a></li>
+                
             </ul>
         </nav>
 
