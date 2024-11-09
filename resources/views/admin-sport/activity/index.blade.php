d406a79a9ef9a7cd089c1035b3206be5441faaf8
Alfeh Rae <chaimelenjambre@gmail.com>
2024-11-09 21:46:28 +0800

    update

--- resources/views/admin-sport/activity/index.blade.php
+++ resources/views/admin-sport/activity/index.blade.php
@@ -365,19 +365,19 @@
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="deleteModalLabel">Delete Modal</h5>
+                <h5 class="modal-title" id="deleteModalLabel">Archive</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <form action="" id="deleteForm" method="POST">
                 @csrf
                 @method('DELETE')
                 <div class="modal-body">
-                    Are you sure you want to delete this?
+                    Are you sure you want to archive this?
                     <input type="hidden" name="status" value="2">
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="submit" class="btn btn-primary">Delete</button>
+                    <button type="submit" class="btn btn-primary">Archive</button>
                 </div>
             </form>
 
